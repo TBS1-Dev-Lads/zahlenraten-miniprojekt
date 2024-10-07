f9cd4ebf16496e07e4dbf818070c562457e2208a
steev <mauricefl@outlook.de>
2024-10-07 08:40:13 +0200

    feat: :sparkles: adding api route for starting a game

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 # app.py
+import datetime
+import random
+import string
 from flask import Flask, send_from_directory, jsonify
 import os
+
+sessions = {
+    "res":int,
+    "lastChanged": datetime.time,
+    "tries": int,
+}
 
 app = Flask(__name__, static_folder='web/dist')
 
@@ -12,9 +21,22 @@ def serve_vue_app():
 def serve_vue_path(path):
     return send_from_directory(app.static_folder, path)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    return jsonify({"message": "Hello from Flask!"})
+@app.route('/api/game/start', methods=['GET'])
+def start_game():
+    # generate sessionid
+    session_id = ''.join(random.choices(string.ascii_letters + string.digits, k=16))
+
+    # generate number
+    random_number = random.randint(1, 100)
+    
+    # create the users session
+    sessions[session_id] = {
+        "res": random_number,
+        "lastChanged": datetime.time.now,
+        "tries": 0,
+    }
+    
+    return jsonify({"status": "ok", "session": session_id})
 
 if __name__ == '__main__':
     app.run(debug=True)
